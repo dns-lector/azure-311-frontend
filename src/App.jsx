bfc983f6c1a988d3e0e888ac01ab9770443c5ef8
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-06-23 20:44:15 +0300

    Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState(null);
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const signInClick = () => {
+    const credentials = btoa(login + ':' + password);
+    console.log(login, password, credentials);
+    fetch("https://pv133od0.azurewebsites.net/Cosmos/SignIn", {
+    // fetch("https://localhost:7224/Cosmos/SignIn", {
+        headers: {
+            'Authorization': 'Basic ' + credentials
+        }
+    }).then(r => r.json())
+        .then(j => {
+            if (j.status.isOk) {
+                console.log(j.data);
+                setToken(j.data);
+                setName( JSON.parse( atob(j.data.split('.')[1]) ).nam );
+            }
+            else {
+                console.error(j);
+            }
+        });
+  };
+
+  const authBlock = () => <>
+      <span>Login:</span><input value={login} onChange={e => setLogin(e.target.value)} />
+      <br/>
+      <span>Password:</span><input type='password' value={password} onChange={e => setPassword(e.target.value)} />        
+      <br/>
+      <button onClick={signInClick}>Sign In</button>
+    </>;
+
+  return <>
+  {!token ? authBlock() : <>
+    <span>Hello, {name}</span>
+    <br/>
+    <button onClick={() => setToken(null)}>Sign Out</button>
+  </>}
+    </>;
 }
 
 export default App
